652997dcbf381e7b450d53d011ea45380bd4d328
Douglas Smith <smithda@gmail.com>
2024-04-22 15:21:31 -0400

    Adding Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,jsx,ts,tsx,md,mdx}',
+    './src/**/*.{js,jsx,ts,tsx,md,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
