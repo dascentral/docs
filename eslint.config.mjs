9725821c2667137b8b0977dad1e3bd85ca35d19b
Douglas Smith <smithda@gmail.com>
2024-04-24 11:14:41 -0400

    WIP

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,41 @@
-// Plugins
-import jest from "eslint-plugin-jest";
-import prettier from "eslint-plugin-prettier";
-import react from "eslint-plugin-react";
+// @ts-check
+
+import eslint from '@eslint/js';
+import deprecationPlugin from 'eslint-plugin-deprecation';
+import jestPlugin from 'eslint-plugin-jest';
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
+// import prettier from "eslint-plugin-prettier";
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import tseslint from 'typescript-eslint';
 
 // Configs
-import prettierConfig from "eslint-config-prettier";
-import reactConfig from "eslint-plugin-react/configs/recommended";
+// import prettierConfig from "eslint-config-prettier";
+// import reactConfig from "eslint-plugin-react/configs/recommended";
 
-module.exports = [
+export default tseslint.config(
   {
-    files: ['**/*.{cjs,js,jsx,mjs,ts,tsx}'],
+    files: ['**/*.{js,jsx,cjs,mjs,ts,tsx}'],
+  },
+  {
     ignores: [
       '.next',
+      '.pnp.*',
       'public/',
     ],
+  },
+  {
     plugins: {
-      jest,
-      prettier,
-      react,
+      ['@typescript-eslint']: tseslint.plugin,
+      ['deprecation']: deprecationPlugin,
+      ['jest']: jestPlugin,
+      ['jsx-a11y']: jsxA11yPlugin,
+      ['react-hooks']: reactHooksPlugin,
+      ['react']: reactPlugin,
     },
-    prettierConfig,
-    reactConfig,
-    rules: {
-      'indent': ['error', 2],
-      'prettier/prettier': ['error', {}, { usePrettierrc: true }],
-    },
+    // rules: {
+    //   'indent': ['error', 2],
+    //   'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    // },
   }
-];
+);
